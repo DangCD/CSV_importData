e5bc8289f77eaf46274740150e6ce08d92831a34
Admin <giapdang188@gmail.com>
2024-11-24 23:35:33 +0700

    Add project files.

---
+++ Test_V4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_V4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           int a ;
+            List<int> b = new List<int>();
+            truyenthamchieu(out a);          
+            int . p
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
